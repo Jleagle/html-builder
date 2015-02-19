30e4e4db25415b2a3037798fc49685d97434e18e
Jleagle <jimeagle@gmail.com>
2015-02-19 23:58:02 +0000

    Add pagination

--- tests/TagTest.php
+++ tests/TagTest.php
@@ -1,4 +1,6 @@
 <?php
+use Jleagle\HtmlBuilder\Bootstrap\Pagination;
+
 class TagTest extends PHPUnit_Framework_TestCase
 {
 
@@ -27,23 +29,54 @@ class TagTest extends PHPUnit_Framework_TestCase
     $audio->setMuted();
     $audio->setLoop();
     $audio->setControls();
-    $this->assertEquals('<audio preload="preload" src="http://source.com" autoplay="autoplay" muted="muted" loop="loop" controls="controls">Content</audio>', (string)$audio);
+    $this->assertEquals(
+      '<audio preload="preload" src="http://source.com" autoplay="autoplay" muted="muted" loop="loop" controls="controls">Content</audio>',
+      (string)$audio
+    );
 
     $audio->setAutoPlay(false);
     $audio->setMuted(false);
     $audio->setLoop(false);
     $audio->setControls(false);
-    $audio->addSource(new \Jleagle\HtmlBuilder\Tags\Source('http://source.com'));
-    $this->assertEquals('<audio preload="preload" src="http://source.com">Content<source src="http://source.com" /></audio>', (string)$audio);
+    $audio->addSource(
+      new \Jleagle\HtmlBuilder\Tags\Source('http://source.com')
+    );
+    $this->assertEquals(
+      '<audio preload="preload" src="http://source.com">Content<source src="http://source.com" /></audio>',
+      (string)$audio
+    );
 
     $ul = new \Jleagle\HtmlBuilder\Tags\Lists\Ul();
     foreach(range(1, 3) as $v)
     {
-      $li = new \Jleagle\HtmlBuilder\Tags\Lists\Li('item '.$v);
+      $li = new \Jleagle\HtmlBuilder\Tags\Lists\Li('item ' . $v);
       $ul->addListItem($li);
     }
-    $this->assertEquals('<ul><li>item 1</li><li>item 2</li><li>item 3</li></ul>', (string)$ul);
+    $this->assertEquals(
+      '<ul><li>item 1</li><li>item 2</li><li>item 3</li></ul>',
+      (string)$ul
+    );
+  }
 
+  public function testPagination()
+  {
+    $pag = new Pagination(1, 3, '/search?page={{page}}');
+    $this->assertEquals(
+      '<nav><ul class="pagination"><li class="disabled"><a>Previous</a></li><li class="active"><a>1</a></li><li><a href="/search?page=2">2</a></li><li><a href="/search?page=3">3</a></li><li><a href="/search?page=2">Next</a></li></ul></nav>',
+      (string)$pag
+    );
+
+    $pag = new Pagination(10, 20, '/search?page={{page}}');
+    $this->assertEquals(
+      '<nav><ul class="pagination"><li><a href="/search?page=9">Previous</a></li><li><a href="/search?page=1">1</a></li><li><a href="/search?page=2">2</a></li><li><a href="/search?page=3">3</a></li><li class="disabled"><span>&hellip;</span></li><li><a href="/search?page=8">8</a></li><li><a href="/search?page=9">9</a></li><li class="active"><a>10</a></li><li><a href="/search?page=11">11</a></li><li><a href="/search?page=12">12</a></li><li class="disabled"><span>&hellip;</span></li><li><a href="/search?page=18">18</a></li><li><a href="/search?page=19">19</a></li><li><a href="/search?page=20">20</a></li><li><a href="/search?page=11">Next</a></li></ul></nav>',
+      (string)$pag
+    );
+
+    $pag = new Pagination(5, 5, '/search?page={{page}}');
+    $this->assertEquals(
+      '<nav><ul class="pagination"><li><a href="/search?page=4">Previous</a></li><li><a href="/search?page=1">1</a></li><li><a href="/search?page=2">2</a></li><li><a href="/search?page=3">3</a></li><li><a href="/search?page=4">4</a></li><li class="active"><a>5</a></li><li class="disabled"><a href="/search?page=6">Next</a></li></ul></nav>',
+      (string)$pag
+    );
   }
 
   public function testException()
@@ -52,5 +85,4 @@ class TagTest extends PHPUnit_Framework_TestCase
     $hr = new \Jleagle\HtmlBuilder\Core\Tag('hr', [], 'content');
     $hr->render();
   }
-
 }
