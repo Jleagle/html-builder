d36768c54df0d100d31bbddca8012f9dd670a9fd
Jleagle <jimeagle@gmail.com>
2015-03-07 14:14:55 +0000

    Input types

--- src/Tags/Forms/Input.php
+++ src/Tags/Forms/Input.php
@@ -5,6 +5,30 @@ use Jleagle\HtmlBuilder\Tags\Abstracts\FormInputAbstract;
 
 class Input extends FormInputAbstract
 {
+  const TYPE_BUTTON = 'button';
+  const TYPE_CHECKBOX = 'checkbox';
+  const TYPE_COLOR = 'color';
+  const TYPE_DATE = 'date';
+  const TYPE_DATETIME = 'datetime';
+  const TYPE_DATETIME_LOCAL = 'datetime-local';
+  const TYPE_EMAIL = 'email';
+  const TYPE_FILE = 'file';
+  const TYPE_HIDDEN = 'hidden';
+  const TYPE_IMAGE = 'image';
+  const TYPE_MONTH = 'month';
+  const TYPE_NUMBER = 'number';
+  const TYPE_PASSWORD = 'password';
+  const TYPE_RADIO = 'radio';
+  const TYPE_RANGE = 'range';
+  const TYPE_RESET = 'reset';
+  const TYPE_SEARCH = 'search';
+  const TYPE_SUBMIT = 'submit';
+  const TYPE_TEL = 'tel';
+  const TYPE_TEXT = 'text';
+  const TYPE_TIME = 'time';
+  const TYPE_URL = 'url';
+  const TYPE_WEEK = 'week';
+
   protected $_tag = 'input';
 
   public function __construct($type, $value)
